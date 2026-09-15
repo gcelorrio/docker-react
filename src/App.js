c023fb89e1a050d5f4feb8eadf967fb9fcce157b
Gabriel Celorrio <gcelorrio@gmail.com>
2026-09-15 13:38:30 +0200

    AWS Deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ceuta es España!
         </a>
       </header>
     </div>
